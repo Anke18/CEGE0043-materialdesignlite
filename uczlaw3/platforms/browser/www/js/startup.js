9e2d460d4cd33023fcaaed4bbd9446c9dcec84c4
Anke18 <uczlaw3@ucl.ac.uk>
2019-02-07 12:33:06 -0500

    Add automatically load function

--- uczlaw3/platforms/browser/www/js/startup.js
+++ uczlaw3/platforms/browser/www/js/startup.js
@@ -3,3 +3,8 @@ function trackAndCircle(){
 	trackLocation();
 	addPointLinePoly();
 }
+
+function startup(){
+	document.addEventListener('DOMContentLoaded',
+	function(){trackAndCircle();}, false);
+}

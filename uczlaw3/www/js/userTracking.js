9ce4d3c2f97450f7be2478b16b80bb7afba24e8c
Anke18 <uczlaw3@ucl.ac.uk>
2019-02-07 14:51:08 -0500

    Add function to get distance from multiple points

--- uczlaw3/www/js/userTracking.js
+++ uczlaw3/www/js/userTracking.js
@@ -18,12 +18,12 @@ function showPosition(position){
 	getDistance();
 }
 
-function getDistance() {
+function getDistance(){
 	//alert('getting distance');
-	navigator.geolocation.getCurrentPosition(getDistanceFromPoint);
+	navigator.geolocation.getCurrentPosition(getDistanceFromMultiplePoints);
 }
 
-function getDistanceFromPoint(position) {
+function getDistanceFromPoint(position){
 	var lat = 51.524479;
 	var lng = -0.133894;
 	var distance = calculateDistance(position.coords.latitude, position.coords.longitude, lat,lng, 'K');
@@ -31,8 +31,23 @@ function getDistanceFromPoint(position) {
 	if (distance < 0.1) {alert('You are close to UCL!');}
 }
 
+function getDistanceFromMultiplePoints(position){
+	var minDistance = 100000000000;
+	var closestQuake = "";
+	for(var i = 0; i < earthquakes.features.length; i++)
+	{
+		var obj = earthquakes.features[i];
+		var distance = calculateDistance(position.coords.latitude,position.coords.longitude,
+		obj.geometry.coordinates[0],obj.geometry.coordinates[1],'K');
+		if (distance < minDistance){
+			minDistance = distance;
+			closestQuake = obj.properties.place;}
+	}
+	alert("Earthquake: " + closestQuake + " is distance " + minDistance + "away");
+}
+
 // code adapted from https://www.htmlgoodies.com/beyond/javascript/calculate-the-distance-between-two-points-inyour-web-apps.html
-function calculateDistance(lat1, lon1, lat2, lon2, unit) {
+function calculateDistance(lat1, lon1, lat2, lon2, unit){
 	var radlat1 = Math.PI * lat1/180;
 	var radlat2 = Math.PI * lat2/180;
 	var radlon1 = Math.PI * lon1/180;
